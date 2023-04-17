2debd54292cda79146807f7e1a855b54cb8a6f66
Phanon <1ahohnm1@gmail.com>
2023-04-17 20:44:44 +0300

    Progrma_Add

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿string[] Array1 = {"Bob", "Constantinopolis", "Antiochia", "Arm", "451", "42"};
+string[] Result = new string [4];
+int j = 0;
+for(int i = 0; i < Array1.Length;i++)
+{
+    if (Array1[i].Length <=3)
+    {
+      Result[j] = Array1[i];
+      j++;
+    }
+}
+var str = string.Join(" ", Result);
+Console.WriteLine(str);
